ace7e1b5758e6ce29c2fdc59d33fd8c52e267a13
Jack Curran <jackurran49@gmail.com>
2023-02-07 17:29:29 -0500

    fixing .sh

--- install_java.sh
+++ install_java.sh
@@ -3,7 +3,9 @@
 sudo yum install -y curl wget
 curl -O https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d/36/GPL/openjdk-18_linux-x64_bin.tar.gz
 tar xvf openjdk-18_linux-x64_bin.tar.gz
-sudo mv jdk-18 /opt/
+sudo mv jdk-18 /opt/ 
+sudo tee /etc/profile.d/jdk18.sh < exports.txt
 export JAVA_HOME=/opt/jdk-18
 export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
 source /etc/profile.d/jdk18.sh
