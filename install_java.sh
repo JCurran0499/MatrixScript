1551d063ddd9b1e1e109aa1caeb7f87e50e59dd8
Jack Curran <jackurran49@gmail.com>
2023-02-07 17:30:35 -0500

    test for java

--- install_java.sh
+++ install_java.sh
@@ -5,7 +5,4 @@ curl -O https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d
 tar xvf openjdk-18_linux-x64_bin.tar.gz
 sudo mv jdk-18 /opt/ 
 sudo tee /etc/profile.d/jdk18.sh < exports.txt
-export JAVA_HOME=/opt/jdk-18
-export PATH=\$PATH:\$JAVA_HOME/bin
-EOF
 source /etc/profile.d/jdk18.sh
