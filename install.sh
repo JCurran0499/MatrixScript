8cbe8f797a96168b7443c8791e7ed1b2e12c5ee7
Jack Curran <jackurran49@gmail.com>
2023-02-20 17:01:50 -0500

    fixing install commands

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 yum update -y
+
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
+cp index.html /var/www/html
+cp frontend.js /var/www/html
 
 sudo yum install -y curl wget
 curl -O https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d/36/GPL/openjdk-18_linux-x64_bin.tar.gz
