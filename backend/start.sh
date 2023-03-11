f9079b691e48712fc531e686bdea9312b8316ec1
Jack Curran <jackurran49@gmail.com>
2023-03-11 12:05:32 -0500

    rebrand

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/MatrixScape-1.0-jar-with-dependencies.jar
+java -jar target/MatrixScript-1.0-jar-with-dependencies.jar
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,38 +8,16 @@ import './App.css';
 function App() {
   const [session, handleSession] = useState('')
 
-  const getCookie = (cookie) => {
-    const cookieList = document.cookie.split(";")
-    for (let i = 0; i < cookieList.length; i++) {
-      let c = cookieList[i].trim()
-      if (c.startsWith(cookie + '='))
-        return c.substring(cookie.length + 1)
-    }
-
-    return ''
-  }
-
   useEffect(() => {
-    const oldToken = getCookie("token")
-    if (oldToken.length > 0) {
-      axios({
-        method: 'delete',
-        url: `http://${process.env.REACT_APP_BACKEND}:4567/token/${oldToken}`
-      })
-    }
-
     axios({
       method: 'get',
       url: `http://${process.env.REACT_APP_BACKEND}:4567/token`
     })
     .then((res) => res.data)
-    .then((json) => {
-      const token = json.sessionToken
-      document.cookie = `token=${token}`
-      return token
-    })
+    .then((json) => json.sessionToken)
     .then(handleSession)
   }, [])
+
 
   return (
     <div className="App">
