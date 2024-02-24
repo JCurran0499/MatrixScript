990b782a98507032b4251e1243e3a65c2ce4cc58
Jack Curran <jackurran49@gmail.com>
2024-02-24 17:16:45 -0500

    updating source folder

--- codedeploy/codedeploy-kill.sh
+++ codedeploy/codedeploy-kill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/ec2-user/MatrixScript || exit
+cd /home/ec2-user/MatrixScript-Backend || exit
 if ps -p "$(<deploy/pid.txt)"
 then
    sudo kill -9 "$(<deploy/pid.txt)"
--- deploy/codedeploy-start.sh
+++ deploy/codedeploy-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/ec2-user/MatrixScript || exit
+cd /home/ec2-user/MatrixScript-Backend || exit
 java -jar target/MatrixScript-1.0-jar-with-dependencies.jar > /dev/null 2> /dev/null < /dev/null &
 echo $! > deploy/pid.txt
